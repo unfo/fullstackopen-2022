9116790c3d5fc3fdb85b6048eb89a580c37006b0
jw <jw@jw.fi>
2022-07-19 12:09:19 +0300

    1.9 uni step4

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -4,9 +4,12 @@ const Statistics = ({good, neutral, bad}) => {
   let total = good + neutral + bad;
   let avg = (total > 0) ? (good + (-1 * bad)) / total : 0;
   let positive = (total > 0) ? 100 * (good / total) : 0;
-
-  return (
-    <>
+  
+  if (total === 0) {
+    return (<p>No feedback given</p>)
+  } else {
+    return (
+      <>
       <h1>statistics</h1>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
@@ -14,26 +17,27 @@ const Statistics = ({good, neutral, bad}) => {
       <p>All {total}</p>
       <p>Avg {avg}</p>
       <p>Positive {positive} %</p>
-    </>
-  )
-}
-
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  // const options = 
-  return (
-    <div>
+      </>
+      )
+    }
+  }
+  
+  const App = () => {
+    // tallenna napit omaan tilaansa
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    
+    // const options = 
+    return (
+      <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
-  )
-}
-
-export default App
+      </div>
+      )
+    }
+    
+    export default App
