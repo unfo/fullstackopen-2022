416a02553c5d238bcf37b7f2a99233812404a3a7
jw <jw@jw.fi>
2022-07-19 13:14:50 +0300

    1.14 anek step3 + refactor

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,5 +1,47 @@
 import { useState } from 'react'
 
+const AnecdoteOfTheDay = ({ anecdotes, votes, current }) => {
+  return (
+    <>
+    <h1>Anecdote of the day</h1>
+    <div>
+    <AnecdoteWithVotes text={anecdotes[current]} votes={votes[current]} />
+    </div>
+    </>
+  )
+
+}
+const AnecdoteWithVotes = ({text, votes}) => {
+  return (
+    <>
+    <p>{text}</p>
+    <p><pre>has {votes} votes</pre></p>
+    </>
+  )
+}
+const MostVoted = ({ anecdotes, votes }) => {
+  let mostVotes = 0;
+  let winner = -1;
+  for (const [anecdote, voteCount] of votes.entries()) { 
+    if (voteCount > mostVotes) {
+      winner = anecdote;
+      mostVotes = voteCount;
+    }
+  }
+  if (mostVotes === 0) {
+    return (
+      <p>Vote for your favorite!</p>
+      )
+  } else {
+    let text = anecdotes[winner];
+    return (
+      <>
+      <h1>Anecdote with the most votes</h1>
+      <AnecdoteWithVotes text={text} votes={mostVotes} />
+      </>
+      )
+  }
+}
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -10,12 +52,12 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ]
-   
+  
   const [selected, setSelected] = useState(0)
-
+  
   const startingVotes = Array(anecdotes.length).fill(0);
   const [votes, setVotes] = useState(startingVotes);
-
+  
   const randomAnecdote = () => {
     console.log('Random wisdom');
     const total = anecdotes.length;
@@ -25,20 +67,20 @@ const App = () => {
     } while (nextAnecdote === selected); // make sure it actually changes
     setSelected(nextAnecdote);
   }
-
+  
   const vote = () => {
     const newVotes = [...votes];
     newVotes[selected] += 1;
     setVotes(newVotes);
   }
   return (
-    <div>
-      <p>{anecdotes[selected]}</p>
-      <p>{votes[selected]} votes</p>
-      <button onClick={vote}>vote</button>
-      <button onClick={randomAnecdote}>next anecdote</button>
-    </div>
-  )
+    <>
+    <AnecdoteOfTheDay anecdotes={anecdotes} votes={votes} current={selected} />
+    <button onClick={vote}>vote</button>
+    <button onClick={randomAnecdote}>next anecdote</button>
+    <MostVoted anecdotes={anecdotes} votes={votes} />
+    </>
+    )
 }
 
 export default App
