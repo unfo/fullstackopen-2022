b1536440cf9276078713c7480494d6256f83f462
jw <jw@jw.fi>
2022-07-19 13:38:31 +0300

    2.1 copy of kurssitiedot baseline

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,57 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  const courses = props.contents.map((item, index)=>{
+    return <Part key={"part-"+index} name={item.name} exercises={item.exercises} />
+  })
+  return (<>{courses}</>)
+}
+
+const Total = (props) => {
+
+  const sum = props.contents.reduce((a,b) => a + b.exercises, 0);
+  return (
+    <div>
+      <p>Number of exercises {sum}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content contents={course.parts} />
+      <Total contents={course.parts} />
+    </div>
+  )
+}
+
+export default App
