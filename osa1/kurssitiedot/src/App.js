2c577436306519c0c9e73ac8d94ee4e8e3fd39ac
jw <jw@jw.fi>
2022-07-19 11:21:15 +0300

    step4 - hupsista, oli jo taulukot ja loopit

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -29,26 +29,25 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  const contents = [
-    part1, part2, part3
-  ];
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <Header course={course} />
-      <Content contents={contents} />
-      <Total contents={contents} />
+      <Content contents={parts} />
+      <Total contents={parts} />
     </div>
   )
 }
