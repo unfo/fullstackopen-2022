117b6115f99ce4a9835f9d13c101f65266c7372f
jw <jw@jw.fi>
2022-07-19 10:47:53 +0300

    refactor

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,3 +1,24 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  const courses = props.contents.map((item, index)=>{
+    return <p key={"course-"+index}>{item[0]} {item[1]}</p>
+  })
+  return (<>{courses}</>)
+}
+
+const Total = (props) => {
+  const sum = props.execercises.reduce((a,b) => a+b, 0);
+  return (
+    <div>
+      <p>Number of exercises {sum}</p>
+    </div>
+  )
+}
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -7,20 +28,17 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
+  const contents = [
+    [part1, exercises1],
+    [part2, exercises2],
+    [part3, exercises3]
+  ];
+  const exercises = [exercises1, exercises2, exercises3];
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content contents={contents} />
+      <Total execercises={exercises} />
     </div>
   )
 }
