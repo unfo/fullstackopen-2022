0ac36cd62e14ff380822aa847258650e9d86c089
jw <jw@jw.fi>
2022-07-28 10:14:46 +0300

    3.2: puhelinluettelon backend step2

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -34,6 +34,12 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 })
 
+app.get('/info', (request, response) => {
+  const count = persons.length;
+  const now = new Date();
+  response.send(`<p>Phonebook has ${count} entries.</p><p>${now}</p>`);
+})
+
 const PORT = 3001
   app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
