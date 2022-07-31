16b6e55e6952a1ac3633c1e6f5de2086f1c5701a
jw <jw@jw.fi>
2022-07-31 09:46:30 +0300

    Heroku stuffs

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -100,7 +100,7 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has ${count} entries.</p><p>${now}</p>`);
 })
 
-const PORT = 3001
-  app.listen(PORT, () => {
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
