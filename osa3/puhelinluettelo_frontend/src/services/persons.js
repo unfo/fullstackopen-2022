e332db7b901afad3f01470d3619a5c06745ca58e
jw <jw@jw.fi>
2022-07-31 10:16:29 +0300

    change  baseurl for part2 + proxy

--- osa3/puhelinluettelo_frontend/src/services/persons.js
+++ osa3/puhelinluettelo_frontend/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://shrouded-crag-73221.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
