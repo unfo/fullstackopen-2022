d7f1a961852b30597a55963afbde23159e2ab2d0
jw <jw@jw.fi>
2022-07-31 17:25:53 +0300

    3.13: puhelinluettelo ja tietokanta, step1 (/:id)

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -51,13 +51,9 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(p => p.id === id);
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
