001ed81de638eaf024d7d68ff73a3248e0b91c89
jw <jw@jw.fi>
2022-08-01 11:01:00 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- osa4/blog/utils/list_helper.js
+++ osa4/blog/utils/list_helper.js
@@ -1,7 +1,15 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs
+    .map(blog => blog.likes)
+    .reduce((total, current) => total + current, 0);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 };
