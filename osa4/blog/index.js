7802fcb193d0bc538113a67744b375b0b25ceedf
jw <jw@jw.fi>
2022-08-01 10:06:41 +0300

    4.1 blogilista, step1

--- osa4/blog/index.js
+++ osa4/blog/index.js
@@ -1,43 +1,46 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require('http');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number
-})
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl);
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
-      response.json(blogs)
-    })
-})
+      response.json(blogs);
+    });
+});
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+  const blog = new Blog(request.body);
 
   blog
     .save()
     .then(result => {
-      response.status(201).json(result)
-    })
-})
+      response.status(201).json(result);
+    });
+});
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const PORT = 3003;
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
