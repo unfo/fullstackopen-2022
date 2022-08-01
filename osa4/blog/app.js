03e3b0cdd89614ad8117f3406710bf71b842bbdd
jw <jw@jw.fi>
2022-08-01 10:16:12 +0300

    work in progress

---
+++ osa4/blog/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+
+require('dotenv').config();
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs);
+    });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result);
+    });
+});
+
+module.exports = app;
