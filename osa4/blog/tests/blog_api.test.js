a1bf261783074ac19905a32d4d612e0e0bef1a37
jw <jw@jw.fi>
2022-08-02 14:29:23 +0300

    4.9*: blogilistan testit, step2

--- osa4/blog/tests/blog_api.test.js
+++ osa4/blog/tests/blog_api.test.js
@@ -26,6 +26,18 @@ describe('4.8: blogilistan testit, step 1', () => {
   });
 });
 
+describe('4.9*: blogilistan testit, step2', () => {
+  test('blog is identified by [id] parameter', async () => {
+    const response = await api.get('/api/blogs');
+    const blogs = response.body;
+    console.log(blogs);
+    const first = blogs[0];
+    console.log(first);
+    expect(first.id).toBeDefined();
+    expect(first._id).not.toBeDefined();
+  });
+});
+
 
 afterAll(() => {
   mongoose.connection.close();
