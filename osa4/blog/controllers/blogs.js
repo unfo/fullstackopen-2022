301b7e7f195ad8a05bea698170a938edb7ceee1f
jw <jw@jw.fi>
2022-08-04 12:44:41 +0300

    4.17: blogilistan laajennus, step5

--- osa4/blog/controllers/blogs.js
+++ osa4/blog/controllers/blogs.js
@@ -1,14 +1,24 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogRouter.get('/', async (_, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body);
+  const author = await User.findOne({});
+  if (!author) {
+    return response.status(400).json({
+      error: 'Please add users first'
+    });
+  }
+  blog.user = author.id;
   const savedBlog = await blog.save();
+  author.blogs = author.blogs.concat(savedBlog._id);
+  await author.save();
   response.status(201).json(savedBlog);
 });
 
