bd22da0dcddeb3ed02b62ec21e268ddd028ed651
jw <jw@jw.fi>
2022-08-06 14:46:17 +0300

    5.7* blogilistan frontend, step7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,34 @@
+import { useState } from 'react';
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [status, setStatus] = useState('closed');
+  const icons = {
+    closed: '▶️',
+    open: '🔽',
+  };
+  const openDetails = (event) => {
+    event.preventDefault();
+    setStatus(status === 'open' ? 'closed' : 'open');
+  };
+  const blogDetails = () => {
+    return (
+      <>
+        <button onClick={(event) => { openDetails(event); }}>{icons[status]} {blog.title}</button>
+        <p>{blog.url}</p>
+        <p>{blog.likes} <button>like</button></p>
+        <p>{blog.author}</p>
+      </>
+    );
+  };
+  const blogSummary = () => (
+    <button onClick={(event) => { openDetails(event); }}>{icons[status]} {blog.title} - {blog.author}</button>
+  );
+
+  return (
+    <div className="blog">
+      {status === 'open' ? blogDetails() : blogSummary() }
+    </div>
+  );
+};
 
 export default Blog;
