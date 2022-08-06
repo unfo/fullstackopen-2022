3b641041ebcc5fc922377afaf5be9fea66ce685b
jw <jw@jw.fi>
2022-08-06 14:58:10 +0300

    5.9: blogilistan frontend, step9

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -151,7 +151,7 @@ const App = () => {
           <BlogForm createBlog={addBlog} />
         </Togglable>
         {
-          blogs.map(blog =>
+          blogs.sort((a,b) => b.likes - a.likes).map(blog =>
             <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />
           )
         }
