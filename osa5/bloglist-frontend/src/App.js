2326ef90b7de55b8c441ca99a212e59bdf27c2d6
jw <jw@jw.fi>
2022-08-06 10:47:43 +0300

    eslint according to my preferences

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
-  }, [])
+    );
+  }, []);
 
   return (
     <div>
@@ -18,7 +18,7 @@ const App = () => {
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,10 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from 'axios';
+const baseUrl = '/api/blogs';
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
+};
 
-export default { getAll }
+const blogService = { getAll };
+export default blogService;
