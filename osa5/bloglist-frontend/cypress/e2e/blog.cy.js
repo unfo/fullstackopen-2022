1d262796fe4d3555bda42f5fc6feb2b3c8a85df4
jw <jw@jw.fi>
2022-08-11 16:11:36 +0300

    5.18: blogilistan end to end -testit, step2

--- osa5/bloglist-frontend/cypress/e2e/blog.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
+  const user = {
+    name: 'jan wikholm',
+    username: 'cypress',
+    password: 'hill' // only for local cypress tests, not a leaked secret.
+  };
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    cy.request('POST', 'http://localhost:3003/api/users/', user);
     cy.visit('http://localhost:3000');
   });
 
@@ -8,5 +14,28 @@ describe('Blog app', function() {
     cy.contains('Login required');
     cy.get('input#username').should('be.visible');
     cy.get('input#password').should('be.visible');
+  });
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('input#username').type(user.username);
+      cy.get('input#password').type(user.password);
+      cy.get('form')
+        .contains('Login')
+        .click()
+        .then(() => {
+          cy.contains(`${user.name} logged in`);
+        });
+    });
+
+    it('fails with wrong credentials', function() {
+      cy.get('input#username').type('groucho');
+      cy.get('input#password').type('swordfish');
+      cy.get('form')
+        .contains('Login')
+        .click()
+        .then(() => {
+          cy.contains('wrong credentials');
+        });
+    });
   });
 });
