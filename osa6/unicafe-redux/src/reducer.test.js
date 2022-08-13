4cbce0a443a8cb687a8a9120a932e1e57fdf0609
jw <jw@jw.fi>
2022-08-13 10:11:07 +0300

    6.1: Unicafe revisited, step1

--- osa6/unicafe-redux/src/reducer.test.js
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -9,12 +9,12 @@ describe('unicafe reducer', () => {
   };
 
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {};
+    let state;
     const action = {
       type: 'DO_NOTHING'
     };
 
-    const newState = counterReducer(undefined, action);
+    const newState = counterReducer(state, action);
     expect(newState).toEqual(initialState);
   });
 
@@ -32,4 +32,50 @@ describe('unicafe reducer', () => {
       bad: 0
     });
   });
+  test('OK is incremented', () => {
+    const action = {
+      type: 'OK'
+    };
+    const state = initialState;
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    });
+  });
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    });
+  });
+  test('reseting to zero works', () => {
+    const action = {
+      type: 'ZERO'
+    };
+    const state = {
+      good: 5,
+      ok: 10,
+      bad: 100
+    };
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    });
+  });
+
 });
