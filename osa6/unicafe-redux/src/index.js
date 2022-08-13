9e9636aa70a39ef1c0f1194fea8dbbb2dbf4e257
jw <jw@jw.fi>
2022-08-13 10:02:10 +0300

    eslint

--- osa6/unicafe-redux/src/index.js
+++ osa6/unicafe-redux/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import reducer from './reducer'
+import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import reducer from './reducer';
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
   const good = () => {
     store.dispatch({
       type: 'GOOD'
-    })
-  }
+    });
+  };
 
   return (
     <div>
@@ -22,13 +22,13 @@ const App = () => {
       <div>ok</div>
       <div>bad</div>
     </div>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 const renderApp = () => {
-  root.render(<App />)
-}
+  root.render(<App />);
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
