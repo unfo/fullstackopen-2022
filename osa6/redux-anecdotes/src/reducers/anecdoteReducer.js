582da59d793f09bad58896efad20392021b58524
jw <jw@jw.fi>
2022-08-17 16:10:30 +0300

    6.15 anekdootit createSlice ekana

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,69 +1,27 @@
-// const anecdotesAtStart = [
-//   'If it hurts, do it more often',
-//   'Adding manpower to a late software project makes it later!',
-//   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-//   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-//   'Premature optimization is the root of all evil.',
-//   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-// ];
+import { createSlice } from '@reduxjs/toolkit';
 
-// const getId = () => (100000 * Math.random()).toFixed(0);
-
-// action creator
-export const voteFor = (id) => {
-  return {
-    type: 'VOTE',
-    data: { id }
-  };
-};
-
-// action creator
-export const newQuote = (quote) => {
-  return {
-    type: 'CREATE',
-    data: quote
-  };
-};
-
-export const setAnecdotes = (anecdotes) => {
-  return  {
-    type: 'SET',
-    data: anecdotes
-  };
-};
-
-// const asObject = (anecdote) => {
-//   return {
-//     content: anecdote,
-//     id: getId(),
-//     votes: 0
-//   };
-// };
-
-// const initialState = anecdotesAtStart.map(asObject);
-
-const anecdoteReducer = (state = [], action) => {
-  console.log('anecdoteReducer', action);
-  switch(action.type) {
-  case 'VOTE': {
-    const id = action.data.id;
-    const item = state.find(quote => quote.id === id);
-    const changedItem = {
-      ...item,
-      votes: item.votes + 1
-    };
-    return state.map(quote => quote.id === id ? changedItem : quote);
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState: [],
+  reducers: {
+    voteFor(state, action) {
+      const id = action.payload.id;
+      const item = state.find(quote => quote.id === id);
+      const changedItem = {
+        ...item,
+        votes: item.votes + 1
+      };
+      return state.map(quote => quote.id === id ? changedItem : quote);
+    },
+    newQuote(state, { payload }) {
+      return [ ...state, payload ];
+    },
+    setAnecdotes(state, { payload }) {
+      console.log('setAnecdotes', payload);
+      return payload;
+    }
   }
-  case 'CREATE': {
-    const quote = action.data;
-    return [...state, quote];
-  }
-  case 'SET': {
-    return action.data;
-  }
-  default:
-    return state;
-  }
-};
+});
 
-export default anecdoteReducer;
+export const { voteFor, newQuote, setAnecdotes } = anecdoteSlice.actions;
+export default anecdoteSlice.reducer;
