4c3605adf9b9f77979bbf489617fab270cbd7131
jw <jw@jw.fi>
2022-08-29 09:42:16 +0300

    7.4: anekdoottisovellus ja hookit step1

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -9,6 +9,8 @@ import {
   useNavigate,
 } from 'react-router-dom';
 
+import { useField } from './hooks';
+
 const anecdotePath = '/anecdotes/:id';
 
 const Menu = () => {
@@ -68,17 +70,18 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  const content = useField('text');
+  const author = useField('text');
+  const info = useField('text');
 
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     });
   };
@@ -89,15 +92,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input { ...content }/>
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input { ...author } />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input { ...info } />
         </div>
         <button>create</button>
       </form>
