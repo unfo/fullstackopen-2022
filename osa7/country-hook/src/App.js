ff9ee416c1ae807994d78a53f2ac05ca4b4f585a
jw <jw@jw.fi>
2022-08-29 10:16:00 +0300

    7.7: country hook

--- osa7/country-hook/src/App.js
+++ osa7/country-hook/src/App.js
@@ -18,7 +18,15 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null);
 
-  useEffect(() => {});
+  useEffect(() => {
+    axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+      .then(response => {
+        setCountry(response.data[0]);
+      })
+      .catch(() => {
+        setCountry('');
+      });
+  }, [name]);
 
   return country;
 };
