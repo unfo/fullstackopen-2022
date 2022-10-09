1222f27a642592d02377e017daef8d3a40ff243f
Jan Wikholm <jw@jw.fi>
2022-10-09 13:33:50 +0300

    utilty to debug login session with jwt

--- osa7/blog/backend/controllers/login.js
+++ osa7/blog/backend/controllers/login.js
@@ -1,7 +1,24 @@
 const bcrypt = require('bcrypt');
+const { response } = require('express');
 const loginRouter = require('express').Router();
 const User = require('../models/user');
 const loginHelper = require('../utils/login_helper.js');
+
+loginRouter.post('/whoami', async (request, response) => {
+  const user = request.user;
+  if (!user) {
+    return response.status(401).end();
+  }
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  };
+  const token = loginHelper.tokenFor(userForToken);
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name });
+});
 
 loginRouter.post('/', async (request, response) => {
   const { username, password } = request.body;
