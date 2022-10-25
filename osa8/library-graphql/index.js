7f1c4b495ed5ba1c7dd9e1fb38c1bf27b2d017b1
Jan Wikholm <jw@jw.fi>
2022-10-25 17:49:05 +0300

    8.4 allBooks author filter

--- osa8/library-graphql/index.js
+++ osa8/library-graphql/index.js
@@ -109,7 +109,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -118,7 +118,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (_, args) => {
+      if (!args.author) {
+        return books;
+      }
+      return books.filter((b) => b.author === args.author);
+    },
     allAuthors: () => authors,
   },
   Author: {
